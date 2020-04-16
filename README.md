67eaa64c68ae111848ec06ff273f9e16732ca451
Robert Deaton <rdeaton@platipy.org>
2020-04-15 22:24:35 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Sarah and Robert's Awesome Game
+
+## Setup
+
+```
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+python3 main.py
+```
+
+## Tutorial
+
+Move your cursor around to see colors.
+
+Press `space` to change the size of the paintbrush.
+
+Press `left shift` to see the current size of the paintbrush in the top left
+white square.
+
+Start the game with `python3 main.py random` to draw with random colors;
+otherwise, the game will draw with a randomly generated gradient sequence.
+
+Press `enter` to stop and start drawing.
+
+Hold `control` to erase.
---
+++ main.py
@@ -0,0 +1,74 @@
+import pygame
+import random
+import sys
+
+SIZE=(1920, 1080)
+
+def clamp(value, lower, upper):
+    return max(min(value, upper), lower)
+
+def clamp2d(point, size):
+    """
+    >>> clamp2d((-1, -1), (10, 10))
+    (0, 0)
+    >>> clamp2d((-100, 100), (10, 10))
+    (0, 10)
+    """
+    return (clamp(point[0], 0, size[0]), clamp(point[1], 0, size[1]))
+
+def init():
+    pygame.init()
+    pygame.display.set_mode(SIZE)
+
+def draw_cursor(surface, size_px, position, color):
+    cursor = pygame.Surface((size_px,size_px))
+    cursor.fill(color)
+    surface.blit(cursor, clamp2d(position, (SIZE[0] - size_px, SIZE[1] - size_px)))
+
+def main():
+    init()
+    sizes = range(3, 60, 7)
+    size_idx = 0
+    shift_held = False
+
+    s = pygame.display.get_surface()
+    draw_cursor(s, sizes[-1], (0, 0), (255, 255, 255))
+
+    color_selector = 0
+    color_inc = random.randint(0, 256) + 256**2 + random.randint(0, 256) + 256 + random.randint(0, 256)
+    drawing = True
+    color_scheme = "random" if sys.argv[-1] == "random" else "gradient"
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYUP and event.key == ord('q'):
+                return
+            if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                size_idx = (size_idx + 1) % len(sizes)
+            if event.type == pygame.KEYUP and event.key == pygame.K_RETURN:
+                drawing = not drawing
+            if event.type == pygame.MOUSEMOTION and drawing:
+                if pygame.key.get_mods() & pygame.KMOD_LCTRL:
+                    color = (0, 0, 0)
+                else:
+                    if color_scheme == "random":
+                        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                    else:
+                        color = (color_selector % (256**3) >> 16, color_selector % (256**2) >> 8, color_selector % 256)
+                        color_selector += color_inc
+                draw_cursor(s, sizes[size_idx], event.pos, color)
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_LSHIFT:
+                shift_held = True
+            elif event.type == pygame.KEYUP and event.key == pygame.K_LSHIFT:
+                shift_held = False
+            elif event.type == pygame.KEYUP and event.key == ord('c'):
+                s.fill((0,0,0))
+                draw_cursor(s, sizes[-1], (0, 0), (255, 255, 255))
+            if shift_held:
+                draw_cursor(s, sizes[size_idx], (0, 0), (0, 255, 0))
+            pygame.display.update()
+            pygame.display.flip()
+            draw_cursor(s, sizes[-1], (0, 0), (255, 255, 255))
+
+
+if __name__ == "__main__":
+    main()
