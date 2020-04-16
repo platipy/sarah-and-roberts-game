47ac39e04e72b318c449964bcf9f764167cf21e3
Sarah <sarahcstringer@gmail.com>
2020-04-16 09:38:51 -0700

    Lint with black.

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import pygame
 import random
 import sys
 
-SIZE=(1920, 1080)
+SIZE = (1920, 1080)
+
 
 def clamp(value, lower, upper):
     return max(min(value, upper), lower)
+
 
 def clamp2d(point, size):
     """
@@ -16,14 +18,17 @@ def clamp2d(point, size):
     """
     return (clamp(point[0], 0, size[0]), clamp(point[1], 0, size[1]))
 
+
 def init():
     pygame.init()
     pygame.display.set_mode(SIZE)
 
+
 def draw_cursor(surface, size_px, position, color):
-    cursor = pygame.Surface((size_px,size_px))
+    cursor = pygame.Surface((size_px, size_px))
     cursor.fill(color)
     surface.blit(cursor, clamp2d(position, (SIZE[0] - size_px, SIZE[1] - size_px)))
+
 
 def main():
     init()
@@ -35,7 +40,13 @@ def main():
     draw_cursor(s, sizes[-1], (0, 0), (255, 255, 255))
 
     color_selector = 0
-    color_inc = random.randint(0, 256) + 256**2 + random.randint(0, 256) + 256 + random.randint(0, 256)
+    color_inc = (
+        random.randint(0, 256)
+        + 256 ** 2
+        + random.randint(0, 256)
+        + 256
+        + random.randint(0, 256)
+    )
     drawing = True
     color_scheme = "random" if sys.argv[-1] == "random" else "gradient"
     while True:
@@ -51,17 +62,25 @@ def main():
                     color = (0, 0, 0)
                 else:
                     if color_scheme == "random":
-                        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                        color = (
+                            random.randint(0, 255),
+                            random.randint(0, 255),
+                            random.randint(0, 255),
+                        )
                     else:
-                        color = (color_selector % (256**3) >> 16, color_selector % (256**2) >> 8, color_selector % 256)
+                        color = (
+                            color_selector % (256 ** 3) >> 16,
+                            color_selector % (256 ** 2) >> 8,
+                            color_selector % 256,
+                        )
                         color_selector += color_inc
                 draw_cursor(s, sizes[size_idx], event.pos, color)
             if event.type == pygame.KEYDOWN and event.key == pygame.K_LSHIFT:
                 shift_held = True
             elif event.type == pygame.KEYUP and event.key == pygame.K_LSHIFT:
                 shift_held = False
-            elif event.type == pygame.KEYUP and event.key == ord('c'):
-                s.fill((0,0,0))
+            elif event.type == pygame.KEYUP and event.key == ord("c"):
+                s.fill((0, 0, 0))
                 draw_cursor(s, sizes[-1], (0, 0), (255, 255, 255))
             if shift_held:
                 draw_cursor(s, sizes[size_idx], (0, 0), (0, 255, 0))
